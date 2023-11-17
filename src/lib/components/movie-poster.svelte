14c17d79dbfde4408e6cd3c703562900a1c7ccba
Kyle Nazario <knazario@bitovi.com>
2023-11-17 12:51:23 -0500

    Separate poster component file

---
+++ src/lib/components/movie-poster.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { TMDBMovie } from '$lib/types/tmdb-movie';
+
+  let { movie } = $props<{ movie: TMDBMovie }>();
+</script>
+
+<div class="poster-list-item">
+  <img
+    src={'https://image.tmdb.org/t/p/original' + movie.poster_path}
+    alt="{movie.title} poster"
+  />
+</div>
+
+<style lang="scss">
+  .poster-list-item {
+    img {
+      height: 40rem;
+    }
+  }
+</style>
