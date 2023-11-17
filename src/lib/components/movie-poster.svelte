03339eabe95ab7f746b85f31e597618bfa77a7a6
Kyle Nazario <knazario@bitovi.com>
2023-11-17 13:10:11 -0500

    Derives poster properties

--- src/lib/components/movie-poster.svelte
+++ src/lib/components/movie-poster.svelte
@@ -1,20 +1,21 @@
 <script lang="ts">
+  import { base } from '$app/paths';
   import type { TMDBMovie } from '$lib/types/tmdb-movie';
 
   let { movie } = $props<{ movie: TMDBMovie }>();
+
+  const src = $derived(
+    'https://image.tmdb.org/t/p/original' + movie.poster_path
+  );
+  const href = $derived(base + '/movies/' + movie.id);
 </script>
 
-<div class="poster-list-item">
-  <img
-    src={'https://image.tmdb.org/t/p/original' + movie.poster_path}
-    alt="{movie.title} poster"
-  />
-</div>
+<a {href}>
+  <img {src} alt="{movie.title} poster" />
+</a>
 
 <style lang="scss">
-  .poster-list-item {
-    img {
-      height: 40rem;
-    }
+  img {
+    height: 40rem;
   }
 </style>
