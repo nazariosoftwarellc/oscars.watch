afaedbe0abd5fe9cd06e61874bf20ec70993d7d5
Kyle Nazario <knazario@bitovi.com>
2023-11-17 14:16:03 -0500

    Banner backdrop

--- src/routes/movies/[id]/+page.svelte
+++ src/routes/movies/[id]/+page.svelte
@@ -2,6 +2,32 @@
   import type { ResolvedMovieDetails } from '$lib/types/resolved-data';
 
   let { data } = $props<{ data: ResolvedMovieDetails }>();
+  const bannerUrl = $derived(
+    'https://image.tmdb.org/t/p/original' + data.movieDetails.backdrop_path
+  );
 </script>
 
-<h2>{data.movieDetails.title}</h2>
+<div id="banner" style="background-image: url('{bannerUrl}')">
+  <h2>{data.movieDetails.title}</h2>
+</div>
+
+<style lang="scss">
+  #banner {
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+    height: 100vh;
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    color: white;
+
+    h2 {
+      background-color: rgba(0, 0, 0, 0.9);
+      padding: 2rem;
+      margin-left: 2rem;
+      margin-top: 4rem;
+      border-radius: 1rem;
+    }
+  }
+</style>
