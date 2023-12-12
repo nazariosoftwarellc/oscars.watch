1b48f12a937c53d782da6b9f05d3617e1faecc13
Kyle Nazario <knazario@bitovi.com>
2023-12-12 16:16:51 -0500

    People page formatting

--- src/routes/people/[id]/+page.svelte
+++ src/routes/people/[id]/+page.svelte
@@ -2,6 +2,7 @@
   import dayjs from 'dayjs';
   import type { ResolvedPersonDetails } from '$lib/types/resolved-data';
   import { tmdbMostKnownRoles } from '$lib/utils';
+  import CalloutBox from '$lib/components/callout-box.svelte';
 
   let { data } = $props<{ data: ResolvedPersonDetails }>();
 
@@ -23,31 +24,45 @@
 </script>
 
 <main>
-  <aside>
-    <h2>{data.details.name}</h2>
-    <img {src} alt={data.details.name} />
-    <div id="bio">
-      <p>Born {birthday} in {data.details.place_of_birth} ({age} years old)</p>
-      <div>Known for acting in:</div>
-      <ul>
-        {#each knownForCredits as credit}
-          <li>
-            <em
-              ><a
-                href={'https://www.themoviedb.org/movie/' + credit.id}
-                target="_blank">{credit.title}</a
-              ></em
-            >
-          </li>
-        {/each}
-      </ul>
-    </div>
-  </aside>
+  <CalloutBox>
+    <aside>
+      <h2>{data.details.name}</h2>
+      <img {src} alt={data.details.name} />
+      <div id="bio">
+        <div>
+          Born {birthday} in {data.details.place_of_birth}
+        </div>
+        <p>{age} years old</p>
+        <h3>Known for</h3>
+        <ul>
+          {#each knownForCredits as credit}
+            <li>
+              <em
+                ><a
+                  href={'https://www.themoviedb.org/movie/' + credit.id}
+                  target="_blank">{credit.title}</a
+                ></em
+              >
+            </li>
+          {/each}
+        </ul>
+      </div>
+    </aside>
+  </CalloutBox>
   <div>main stuff</div>
 </main>
 
 <style lang="scss">
+  @use 'src/app' as globals;
+
   main {
     display: flex;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+    padding: globals.$main-spacing;
+
+    @media screen and (max-width: globals.$mobile) {
+      flex-direction: column;
+    }
   }
 </style>
