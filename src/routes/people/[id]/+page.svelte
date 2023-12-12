aa15948fcf4d24a8377cac53c3eed75e8f55684e
Kyle Nazario <knazario@bitovi.com>
2023-12-12 14:56:06 -0500

    Handles crew as well as cast

--- src/routes/people/[id]/+page.svelte
+++ src/routes/people/[id]/+page.svelte
@@ -18,10 +18,31 @@
 
   let knownForCredits = $derived(
     (() => {
-      return data.credits.cast
+      let knownForJobTitle = 'Actor';
+      switch (data.details.known_for_department) {
+        case 'Acting':
+          knownForJobTitle = 'Actor';
+          break;
+        case 'Directing':
+          knownForJobTitle = 'Director';
+          break;
+        case 'Production':
+          knownForJobTitle = 'Producer';
+          break;
+        case 'Writing':
+          knownForJobTitle = 'Writer';
+          break;
+      }
+      let jobs = data.credits.crew.filter(
+        credit => credit.job === knownForJobTitle
+      );
+      if (knownForJobTitle === 'Actor') {
+        jobs = jobs.concat(data.credits.cast);
+      }
+      return jobs
         .filter(credit => new Date(credit.release_date) < now)
         .sort((a, b) => {
-          return b.popularity - a.popularity;
+          return b.vote_count - a.vote_count;
         })
         .filter(credit => credit.vote_average > 7)
         .slice(0, 5);
@@ -34,7 +55,7 @@
   <img {src} alt={data.details.name} />
   <div id="bio">
     <p>Born {birthday} in {data.details.place_of_birth} ({age} years old)</p>
-    <div>Known for:</div>
+    <div>Known for acting in:</div>
     <ul>
       {#each knownForCredits as credit}
         <li>
