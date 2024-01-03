49cfa8db71a1123b60ae4dd895335d4e5d7e892c
Kyle Nazario <knazario@bitovi.com>
2024-01-03 11:01:45 -0500

    Footer component

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { base } from '$app/paths';
+
+  const creditsHref = `${base}/credits`;
+</script>
+
+<hr />
+<footer>
+  <div>
+    <a href={creditsHref}>Credits</a>
+  </div>
+  <div>
+    Created by <a href="https://kylenazario.com" target="_blank">Kyle Nazario</a
+    >
+  </div>
+</footer>
+
+<style lang="scss">
+  @use 'src/app' as globals;
+  footer {
+    padding: 0 globals.$main-spacing;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import Footer from '$lib/components/footer.svelte';
   import Header from '$lib/components/header.svelte';
 </script>
 
 <Header />
 <main>
-    <slot></slot>
+  <slot />
 </main>
+
+<Footer />
