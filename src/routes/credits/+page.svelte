936e2251c2c2941942b598395fc9f2d4c96bd6d1
Kyle Nazario <knazario@bitovi.com>
2024-01-03 11:06:12 -0500

    Credits page

---
+++ src/routes/credits/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts"></script>
+
+<article>
+  <h2>Credits</h2>
+  <p>
+    Created by <a href="https://kylenazario.com" target="_blank">Kyle Nazario</a
+    >.
+  </p>
+  <p>
+    Data and images provided by <a
+      href="https://www.themoviedb.org/"
+      target="_blank">The Movie Database</a
+    >.
+  </p>
+  <p>
+    Read the <a
+      href="https://github.com/nazariosoftwarellc/oscars.watch"
+      target="_blank">source code</a
+    > on GitHub.
+  </p>
+</article>
+
+<style lang="scss">
+  @use 'src/app' as globals;
+  article {
+    padding: 0 globals.$main-spacing;
+  }
+</style>
