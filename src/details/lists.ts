c241b95dae0c58b4b6887712882ed28dc372920e
Kyle Nazario <knazario@bitovi.com>
2024-01-03 11:54:28 -0500

    Homepage list

---
+++ src/details/lists.ts
@@ -0,0 +1,12 @@
+import type { HomepageList } from '$lib/types/homepage-list';
+
+const homepageLists: HomepageList[] = [
+  {
+    name: 'Best Picture',
+    ids: [
+      49046, 76600, 614934, 545611, 674324, 804095, 361743, 497828, 817758,
+      777245
+    ],
+    type: 'movie'
+  }
+];
---
+++ src/lib/types/homepage-list.ts
@@ -0,0 +1,5 @@
+export type HomepageList = {
+  name: string;
+  ids: number[];
+  type: 'movie' | 'person';
+};
