be500c217698ac211d2dfce7396123ab5a95df42
Kyle Nazario <knazario@bitovi.com>
2024-01-11 15:27:37 -0500

    People lists

--- src/details/lists.ts
+++ src/details/lists.ts
@@ -1,5 +1,12 @@
-import type { HomepageMovieList } from '$lib/types/homepage-list';
+import type {
+  HomepageMovieList,
+  HomepagePersonList as HomepagePeopleList
+} from '$lib/types/homepage-list';
 
 export const homepageMovieLists: HomepageMovieList[] = [
   { tmdbId: 8275755, internalLabel: 'best picture' }
+];
+
+export const homepagePeopleLists: HomepagePeopleList[] = [
+  { name: 'Best Actress', ids: [1812] }
 ];
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,19 +1,28 @@
 import TMDBConnector from '$lib/tmdb-connector';
 import type { ResolvedOscarCategoryList } from '$lib/types/resolved-data';
 import type { TMDBList } from '$lib/types/tmdb-list';
-import { homepageMovieLists } from '../details/lists';
+import { homepageMovieLists, homepagePeopleLists } from '../details/lists';
 
 export async function load(): Promise<ResolvedOscarCategoryList> {
-  let tmdbMovieLists: TMDBList[] = [];
+  const tmdbMovieLists: TMDBList[] = [];
   for await (const movieList of homepageMovieLists) {
     tmdbMovieLists.push(await TMDBConnector.getList(movieList.tmdbId));
   }
+  const movieCategories = tmdbMovieLists.map(list => {
+    return {
+      name: list.name,
+      movies: list.items
+    };
+  });
+
+  const tmdbPeople: TMDBPersonDetails[] = [];
+  for await (const peopleList of homepagePeopleLists) {
+    for (const personId of peopleList.ids) {
+      tmdbPeople.push(await TMDBConnector.getPersonDetails(personId));
+    }
+  }
+
   return {
-    categories: tmdbMovieLists.map(list => {
-      return {
-        name: list.name,
-        movies: list.items
-      };
-    })
+    categories: movieCategories
   };
 }
