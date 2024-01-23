206388c291b16dda10e3fe5ec1ebfd17a2116680
Kyle Nazario <knazario@bitovi.com>
2024-01-23 09:56:27 -0500

    2024 movie lists

--- src/details/lists.ts
+++ src/details/lists.ts
@@ -4,7 +4,23 @@ import type {
 } from '$lib/types/homepage-list';
 
 export const homepageMovieLists: HomepageMovieList[] = [
-  { tmdbId: 8275755, internalLabel: 'best picture' }
+  { tmdbId: 8288577, internalLabel: 'picture' },
+  { tmdbId: 8288578, internalLabel: 'adapted screenplay' },
+  { tmdbId: 8288579, internalLabel: 'original screenplay' },
+  { tmdbId: 8288580, internalLabel: 'score' },
+  { tmdbId: 8288581, internalLabel: 'international feature' },
+  { tmdbId: 8288582, internalLabel: 'animated feature' },
+  { tmdbId: 8288583, internalLabel: 'documentary feature' },
+  { tmdbId: 8288584, internalLabel: 'costume' },
+  { tmdbId: 8288585, internalLabel: 'makeup' },
+  { tmdbId: 8288586, internalLabel: 'production design' },
+  { tmdbId: 8288587, internalLabel: 'sound' },
+  { tmdbId: 8288588, internalLabel: 'editing' },
+  { tmdbId: 8288589, internalLabel: 'cinematography' },
+  { tmdbId: 8288590, internalLabel: 'visual effects' },
+  { tmdbId: 8288591, internalLabel: 'live short' },
+  { tmdbId: 8288592, internalLabel: 'animated short' },
+  { tmdbId: 8288593, internalLabel: 'documentary short' }
 ];
 
 export const homepagePeopleLists: HomepagePeopleList[] = [
