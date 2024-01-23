7db5454b19bf1dce96625f912483c1cda86334d0
Kyle Nazario <knazario@bitovi.com>
2024-01-23 12:40:42 -0500

    Scrolls left when it finishes

--- src/lib/auto-scroll-element.ts
+++ src/lib/auto-scroll-element.ts
@@ -1,6 +1,7 @@
 export function autoScrollElement(element: HTMLElement): void {
   let interval: NodeJS.Timeout | undefined;
   const scrollStartDelay = Math.random() * 1000;
+  let scrollingRight = true;
 
   function stopScrolling() {
     clearInterval(interval);
@@ -8,9 +9,14 @@ export function autoScrollElement(element: HTMLElement): void {
 
   setTimeout(() => {
     interval = setInterval(() => {
-      if (element) {
-        element.scrollLeft += 1;
+      if (element.scrollLeft >= element.scrollWidth - element.clientWidth) {
+        scrollingRight = false;
       }
+      if (element.scrollLeft <= 0) {
+        scrollingRight = true;
+      }
+      const scrollAmount = scrollingRight ? 1 : -1;
+      element.scrollLeft += scrollAmount;
     }, 100)
 
     element.addEventListener('wheel', stopScrolling);
